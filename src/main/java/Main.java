5a18bcfb67b63f74ba0e7b0bf7ea656bb1de52f3
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-03-20 13:58:14 +0000

    2_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner scanner = new Scanner(System.in);
+    int min = Integer.MAX_VALUE;
 
+    System.out.println("Wpisz 5 liczb:");
+
+    for (int i = 0; i < 5; i++) {
+      System.out.print("Wpisz liczbe " + (i + 1) + ": ");
+      int num = scanner.nextInt();
+
+      if (num < min) {
+        min = num;
+      }
+    }
+
+    System.out.println("Minimalna wartosc: " + min);
+    scanner.close();
+  }
 }
